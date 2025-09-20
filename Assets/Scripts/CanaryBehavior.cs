b781c417e7e7f7d9f0cb59bebe933d32c3034d13
Karkaranos <karkaranosyt@gmail.com>
2025-09-19 20:19:55 -0500

    Gizmo

---
+++ Assets/Scripts/CanaryBehavior.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CanaryBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}

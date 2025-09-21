24508154bd4c2e7b840996a84d38752a9ddc3c32
IceyFox006 <cgreenan@mail.bradley.edu>
2025-09-20 21:03:37 -0500

    winlose

--- Assets/Scripts/THESTONE.cs
+++ Assets/Scripts/THESTONE.cs
@@ -3,11 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class THESTONE : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMovement>() == null)
             return;
-        Debug.Log("WIN");
         StaticData.EndID = 1;
         SceneManager.LoadScene("EndScreen");
     }

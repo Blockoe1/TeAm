de193afb758700954806ce4c553a3d9b646a2c84
Karkaranos <karkaranosyt@gmail.com>
2025-09-21 10:10:22 -0500

    Ores break

--- Assets/Scripts/Breakables/BreakableObject.cs
+++ Assets/Scripts/Breakables/BreakableObject.cs
@@ -4,6 +4,8 @@ public class BreakableObject : MonoBehaviour
 {
     [SerializeField] private ScoreObject score;
     [SerializeField] private GameObject particles;
+    [SerializeField] private Sprite[] breakStates = new Sprite[3];
+    [SerializeField] private SpriteRenderer breakRenderer;
 
     private int hits = 0;
     private void Start()
@@ -24,6 +26,19 @@ public class BreakableObject : MonoBehaviour
         if(hits > 0)
         {
             hits--;
+            float percent = hits/(float)score.HitsToBreak;
+            if(percent > .5f && percent < .8f)
+            {
+                breakRenderer.sprite = breakStates[0];
+            }
+            else if (percent >= .3f)
+            {
+                breakRenderer.sprite = breakStates[1];
+            }
+            else if(percent < .3f)
+            {
+                breakRenderer.sprite = breakStates[2];
+            }
             return;
         }
 

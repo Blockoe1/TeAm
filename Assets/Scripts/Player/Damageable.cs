067b38da8b943cb60558cf036a22a7a8aee8bb12
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-20 19:24:58 -0500

    Update Gas Damage

---
+++ Assets/Scripts/Player/Damageable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    [SerializeField] private bool updatePlayerHealth;
+    [SerializeField] private UnityEvent<int> OnDamageEvent;
+    [SerializeField] private UnityEvent OnDeathEvent;
+
+    [SerializeField] private int health;
+
+    private void Awake()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        OnDamageEvent?.Invoke(health);
+        if (updatePlayerHealth)
+        {
+            PlayerStatScript.RemainingHealth = health;
+        }
+        if (health <= 0)
+        {
+            OnDeathEvent?.Invoke();
+        }
+    }
+}
